8186ee4dd9fe048b4a48efcaa58d7a6517fcff00
CarlieMartece <carliemartece@gmail.com>
2023-06-05 09:44:57 +0100

    reformat chroma and cnmc dev data from json to js

---
+++ db/dev-data/chromaSphereSystem.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    name: "Hew Espireth",
+    anagram: "White Sphere",
+    citizens: "Heshpers",
+  },
+  {
+    name: "Evol Espireth",
+    anagram: "Violet Sphere",
+    citizens: "Evollish",
+  },
+  {
+    name: "Godshi Enpire",
+    anagram: "Indigo Sphere",
+    citizens: "Godish",
+  },
+  {
+    name: "Bure Eshpel",
+    anagram: "Blue Sphere",
+    citizens: "Beshpers",
+  },
+  {
+    name: "Geren Eshper",
+    anagram: "Green Sphere",
+    citizens: "Grenish",
+  },
+  {
+    name: "Pyro Eshwelle",
+    anagram: "Yellow Sphere",
+    citizens: "Pyrish",
+  },
+  {
+    name: "Opree Shengra",
+    anagram: "Orange Sphere",
+    citizens: "Oprish",
+  },
+  {
+    name: "Pred Heres",
+    anagram: "Red Sphere",
+    citizens: "Predian",
+  },
+  {
+    name: "Peblash Reck",
+    anagram: "Black Sphere",
+    citizens: "Reckish",
+  },
+];
